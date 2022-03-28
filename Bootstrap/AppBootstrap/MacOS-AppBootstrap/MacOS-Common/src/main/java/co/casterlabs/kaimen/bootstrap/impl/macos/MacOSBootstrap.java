e0d1b7b09b7462cf182fb3edac7ba565f10b2999
Ender <33337309+e3ndr@users.noreply.github.com>
2022-03-28 10:58:21 -0500

    Restructure the project.

---
+++ Bootstrap/AppBootstrap/MacOS-AppBootstrap/MacOS-Common/src/main/java/co/casterlabs/kaimen/bootstrap/impl/macos/MacOSBootstrap.java
@@ -0,0 +1,52 @@
+package co.casterlabs.kaimen.bootstrap.impl.macos;
+
+import java.net.URL;
+
+import org.eclipse.swt.internal.cocoa.OS;
+import org.eclipse.swt.widgets.Display;
+import org.jetbrains.annotations.Nullable;
+
+import co.casterlabs.kaimen.app.App;
+import co.casterlabs.kaimen.util.threading.MainThread;
+import co.casterlabs.kaimen.webview.Webview;
+import co.casterlabs.kaimen.webview.impl.webkit.WkWebview;
+import lombok.NonNull;
+
+public class MacOSBootstrap extends App {
+
+    public MacOSBootstrap() {
+        MainThread.submitTask(() -> {
+            // Init the display for the main thread.
+            // We need to create atleast one display for OS.setTheme() to work.
+            new Display();
+        });
+    }
+
+    @Override
+    protected void setName0(@NonNull String name) {
+        Display.setAppName(name);
+
+        for (Webview wv : Webview.getActiveWebviews()) {
+            if (wv instanceof WkWebview) {
+                ((WkWebview) wv).updateTitle();
+            }
+        }
+    }
+
+    @Override
+    protected void setAppearance0(@NonNull Appearance appearance) {
+        MainThread.submitTask(() -> {
+            OS.setTheme(appearance.isDark());
+        });
+    }
+
+    @Override
+    protected void setIcon0(@Nullable URL url) {
+        for (Webview wv : Webview.getActiveWebviews()) {
+            if (wv instanceof WkWebview) {
+                ((WkWebview) wv).changeImage(url);
+            }
+        }
+    }
+
+}
