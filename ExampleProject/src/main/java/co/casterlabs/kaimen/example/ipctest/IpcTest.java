e23576f43c2bc03263d57e94234660d0fc1b5dec
Ender <33337309+e3ndr@users.noreply.github.com>
2022-10-02 05:20:46 -0500

    Remove the IPC code, it's been moved to Casterlabs/Commons.

--- ExampleProject/src/main/java/co/casterlabs/kaimen/example/ipctest/IpcTest.java
+++
@@ -1,9 +0,0 @@
-package co.casterlabs.kaimen.example.ipctest;
-
-public class IpcTest {
-
-    public static void main(String[] args) throws Exception {
-        TestHost test = new TestHost();
-    }
-
-}
